3bdf0819a7609f161d641eea0f8bd98fb03f22ca
Musrat Ali <168627051+musratali@users.noreply.github.com>
2024-06-27 12:21:06 +0500

    commit 3

---
+++ ap.js
@@ -0,0 +1,14 @@
+// function mpt(){
+//     document.getElementById("a")
+// }
+function cal(val){
+    document.getElementById('a').value +=val;
+}
+function mpt(){
+    document.getElementById("a").value = " ";
+}
+function solution() {
+    let a = document.getElementById('a').value;
+    let b = eval(a);
+    document.getElementById('a').value = b
+}
